d21dc7af1c1f3ed47144329acfd8a202b48e106c
Martin Wittwer <95050816+marwit94@users.noreply.github.com>
2022-01-16 14:29:40 +0100

    Programm Ultraschallsensor

---
+++ programm_distanzsensor.ino
@@ -0,0 +1,15 @@
+#include <Ultrasonic.h>
+Ultrasonic ultrasonic (10);
+
+void setup (){
+  Serial.begin(38400);
+  }
+  
+
+void loop ()
+  {
+  long range;
+  range = ultrasonic.MeasureInMillimeters();  
+
+  Serial.println(range);
+  }
